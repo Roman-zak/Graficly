2de2a374986c7e714fdcd8bb0fc4d2c7c7f46ed9
Vollframiy <vladyslav.hladkyi@redtag.pro>
2022-10-24 21:56:23 +0300

    changed from  canvas to img

--- src/modules/my/colors/colors.js
+++ src/modules/my/colors/colors.js
@@ -2,7 +2,6 @@ import { LightningElement, track } from 'lwc';
 
 export default class Colors extends LightningElement {
     @track isShowInfo = false;
-    @track index = 0;
     @track prevBrightnessValue = 0;
 
     handleShowInformation(){
@@ -14,47 +13,39 @@ export default class Colors extends LightningElement {
     }
 
     handleChangeBrightness(event){
-        let offset = event.target.value - this.prevBrightnessValue;
-        
-        console.log(offset);
+        console.log(event.target.value);
 
-        let canvas = this.template.querySelector('[data-id="display-canvas"]');
+        let img = this.template.querySelector('[data-id="display-image"]');
+        let w = img.naturalWidth;
+        let h = img.naturalHeight;
+        let canvas = document.createElement('canvas');
         let context = canvas.getContext('2d');
-        let w = canvas.width;
-        let h = canvas.height
+
+        canvas.height = h;
+        canvas.width = w;
+        context.drawImage(img, 0, 0);
+
         let originalPixels = context.getImageData(0, 0, w, h);
         let currentPixels = context.getImageData(0, 0, w, h);
 
         for(let I = 0, L = originalPixels.data.length; I < L; I += 4)
         {
-            if(originalPixels.data[I] + offset <= 255 && originalPixels.data[I] + offset >= 0 &&
-                originalPixels.data[I + 1] + offset <= 255 && originalPixels.data[I + 1] + offset >= 0 &&
-                originalPixels.data[I + 2] + offset <= 255 && originalPixels.data[I + 2] + offset >= 0)
-            {
-                currentPixels.data[I] = originalPixels.data[I] + offset;
-                currentPixels.data[I + 1] = originalPixels.data[I + 1] + offset;
-                currentPixels.data[I + 2] = originalPixels.data[I + 2] + offset;
-                //currentPixels.data[I + 3] = event.target.value;
-            }
+            currentPixels.data[I] = originalPixels.data[I] + 5;
+            currentPixels.data[I + 1] = originalPixels.data[I + 1] + 5;
+            currentPixels.data[I + 2] = originalPixels.data[I + 2] + 5
         }
 
-        console.log(originalPixels.data[0]);
-
         context.putImageData(currentPixels, 0, 0);
+        img.src = canvas.toDataURL();
     }
 
     handleLoadImg(event){
         let reader = new FileReader();
-        reader.readAsDataURL(event.target.files[this.index++]);
+        reader.readAsDataURL(event.target.files[0]);
 
         reader.onload = () => {
             this.template.querySelector('[data-id="display-image"]').src = reader.result;
-            let canvas = this.template.querySelector('[data-id="display-canvas"]');
-            let context = canvas.getContext('2d');
-            let w = canvas.width;
-            let h = canvas.height
-
-            context.drawImage(this.template.querySelector('[data-id="display-image"]'), 0, 0, w, h);
+            this.template.querySelector('[data-id="display-image2"]').src = reader.result;
         };
     }
 }
